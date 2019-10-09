1c1472bb99b9b6f33d4157d6078cc9ff63ebb964
cryptotoad <cryptotoad@users.noreply.github.com>
2019-10-09 01:25:51 -0700

    fixed UD crash bug

--- src/commands/urbandictionary.js
+++ src/commands/urbandictionary.js
@@ -18,7 +18,8 @@ module.exports = {
         .end((err, res) => {
           if (!err && res.status === 200) {
             const uD = res.body
-            if (uD.result_type !== 'no_results' || uD.list.length > 0) {
+            console.log(uD.list.length);
+            if (uD.result_type !== 'no_results' && uD.list.length > 0) {
               msg.channel.createMessage({
                 embed: {
                   color: 0x6832e3,
