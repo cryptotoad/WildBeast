e99b8dc5acb2c448072d7ecbf97b86f48a24551f
cryptotoad <cryptotoad@users.noreply.github.com>
2019-10-09 10:54:52 -0700

    Recursive command directory scanning

--- src/internal/command-indexer.js
+++ src/internal/command-indexer.js
@@ -12,7 +12,7 @@ const aliases = new Map()
 
 for (const cmd in commands) {
   if (result[cmd]) global.logger.error(`Unable to register command ${cmd}, a command with this name already exists.`, true)
-  result[cmd] = commands[cmd]
+    result[cmd] = commands[cmd]
   if (commands[cmd].meta.alias) {
     for (const x of commands[cmd].meta.alias) {
       if (commands[x]) global.logger.error(`Cannot set alias ${x}, there's a command with this name.`, true)
